3d6bf92a38e1a9cb21fc1aac4a22b9cc12c05f36
wangxi0208 <WANX1_15@uni.worc.ac.uk>
2016-10-20 15:36:15 +0100

    111

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,13 @@
 function onLoad()
 {
     
-alert("hello");
+var key = “pen”;
+var value = “blue”;
+window.localStorage.setItem( key, value );
+
+var hello = window.lacalstorage.getItem(key);
+    
+alert(hello);
     
     
 }
